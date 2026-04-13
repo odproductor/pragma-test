8174a11b5f62779eae0c60c96b48612ee8f893d1
odproductor <soouselab@gmail.com>
2026-04-13 09:19:15 -0500

    Upload Project Test-Pragma

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'REPLACE_API_KEY',
-    authDomain: 'REPLACE.firebaseapp.com',
-    projectId: 'REPLACE_PROJECT_ID',
-    storageBucket: 'REPLACE.appspot.com',
-    messagingSenderId: 'REPLACE_SENDER_ID',
-    appId: 'REPLACE_APP_ID'
+    apiKey: 'AIzaSyAwnqzDnm_S3gzXQpmZEgONfbfhSb0tBto',
+    authDomain: 'pragma-test-c56a5.firebaseapp.com',
+    projectId: 'pragma-test-c56a5',
+    storageBucket: 'pragma-test-c56a5.firebasestorage.app',
+    messagingSenderId: '917104514890',
+    appId: '1:917104514890:web:8992cec7985c238245d3b3',
+    measurementId: 'G-NKFVMZHRXR'
   },
   flags: {
     categoriesEnabled: true
